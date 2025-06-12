f07b0925ae835cfcc901b2704e640f4f2a9800c1
Tyler Yeager <wow_d2@hotmail.com>
2025-06-11 19:54:34 -0700

    OutputMessage set up

--- src/main/java/com/example/chat/OutputMessage.java
+++ src/main/java/com/example/chat/OutputMessage.java
@@ -1,4 +1,30 @@
 package com.example.chat;
 
+import java.time.LocalTime;
+
 public class OutputMessage {
+    private String from;
+    private String content;
+    private String time;
+
+    public OutputMessage() {}
+    public OutputMessage(String from, String content, String time) {
+        this.from = from;
+        this.content = content;
+        this.time = time;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+
 }
