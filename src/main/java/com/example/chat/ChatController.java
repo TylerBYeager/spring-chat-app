ba28ca94284880d8995eeac9f7d787a79751352d
Tyler Yeager <wow_d2@hotmail.com>
2025-06-11 19:58:53 -0700

    ChatController set up

--- src/main/java/com/example/chat/ChatController.java
+++ src/main/java/com/example/chat/ChatController.java
@@ -1,4 +1,24 @@
 package com.example.chat;
 
+import org.springframework.cglib.core.Local;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Controller
 public class ChatController {
+
+    @MessageMapping("/chat") //from client: /app/chat
+    @SendTo("/topic/messages") // broadcasts to:/topic/messages
+    public OutputMessage send(Message message) {
+        return new OutputMessage(
+                message.getFrom(),
+                message.getContent(),
+                LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))
+        );
+    }
+
 }
