39938d27e6aa3f203aa5f608a454223e18c946a7
Tyler Yeager <wow_d2@hotmail.com>
2025-06-11 19:51:47 -0700

    Message set up

--- src/main/java/com/example/chat/Message.java
+++ src/main/java/com/example/chat/Message.java
@@ -1,4 +1,29 @@
 package com.example.chat;
 
 public class Message {
+        private String from;
+        private String content;
+
+        public Message() {}
+        public Message(String from, String content) {
+            this.from = from;
+            this.content = content;
+        }
+
+        public String getFrom() {
+            return from;
+        }
+
+        public void setFrom(String from) {
+            this.from = from;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
 }
