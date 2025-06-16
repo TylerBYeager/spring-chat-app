18f34bcd150cda7b707ae0bb57a9dc7cad700f9f
Tyler Yeager <wow_d2@hotmail.com>
2025-06-16 16:43:04 -0700

    Updated controller as the Join message wasn't firing.

--- src/main/java/com/example/chat/ChatController.java
+++ src/main/java/com/example/chat/ChatController.java
@@ -1,10 +1,10 @@
 package com.example.chat;
 
-import org.springframework.cglib.core.Local;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalTime;
@@ -13,15 +13,31 @@ import java.time.format.DateTimeFormatter;
 @Controller
 public class ChatController {
 
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public ChatController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
     @MessageMapping("/chat") // from client: /app/chat
-    @SendTo("/topic/messages") // broadcasts to: /topic/messages
+    @SendTo("/topic/messages") // return value goes here
     public Message send(@Payload Message message, SimpMessageHeaderAccessor headerAccessor) {
-        // Save username to WebSocket session
+        String time = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        message.setTime(time);
+
         if (headerAccessor.getSessionAttributes().get("username") == null && message.getFrom() != null) {
             headerAccessor.getSessionAttributes().put("username", message.getFrom());
+
+            // Broadcast JOIN message when first connecting
+            Message joinMessage = new Message(
+                    "System",
+                    message.getFrom() + " has joined the chat",
+                    time,
+                    MessageType.JOIN
+            );
+            messagingTemplate.convertAndSend("/topic/messages", joinMessage);
         }
 
-        message.setTime(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
         message.setType(MessageType.CHAT);
         return message;
     }
@@ -31,5 +47,4 @@ public class ChatController {
     public String typing(@Payload String from) {
         return from + " is typing...";
     }
-
 }
