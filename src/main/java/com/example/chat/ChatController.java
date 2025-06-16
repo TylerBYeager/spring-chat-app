75db74caa587a599cb265e768ba085f1eb2ba623
Tyler Yeager <wow_d2@hotmail.com>
2025-06-16 16:34:27 -0700

    Altered controller messageOutput

--- src/main/java/com/example/chat/ChatController.java
+++ src/main/java/com/example/chat/ChatController.java
@@ -4,6 +4,7 @@ import org.springframework.cglib.core.Local;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalTime;
@@ -12,14 +13,17 @@ import java.time.format.DateTimeFormatter;
 @Controller
 public class ChatController {
 
-    @MessageMapping("/chat") //from client: /app/chat
-    @SendTo("/topic/messages") // broadcasts to:/topic/messages
-    public OutputMessage send(Message message) {
-        return new OutputMessage(
-                message.getFrom(),
-                message.getContent(),
-                LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))
-        );
+    @MessageMapping("/chat") // from client: /app/chat
+    @SendTo("/topic/messages") // broadcasts to: /topic/messages
+    public Message send(@Payload Message message, SimpMessageHeaderAccessor headerAccessor) {
+        // Save username to WebSocket session
+        if (headerAccessor.getSessionAttributes().get("username") == null && message.getFrom() != null) {
+            headerAccessor.getSessionAttributes().put("username", message.getFrom());
+        }
+
+        message.setTime(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        message.setType(MessageType.CHAT);
+        return message;
     }
 
     @MessageMapping("/typing")
